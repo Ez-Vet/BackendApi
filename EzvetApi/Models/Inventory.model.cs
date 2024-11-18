ad933d25edeabdb63e7fdea41ebfe8ef5167ad26
sebasepe <u202125968@upc.edu.pe>
2024-11-18 03:19:53 -0500

    Eliminando contenido de la rama feature/notificaciones

--- EzvetApi/Models/Inventory.model.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EzvetApi.Models;
-
-public class Inventory
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Type { get; set; }
-    public int Quantity { get; set; }
-}
